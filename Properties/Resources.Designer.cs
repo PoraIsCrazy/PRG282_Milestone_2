da1a392633f55917ed18247f0df07f33e1d878a8
Miguel Correia <correiapora@gmail.com>
2021-10-12 21:50:47 +0200

    Registration Form Just needs code

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace PRG282_Milestone_2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap FormLogo {
+            get {
+                object obj = ResourceManager.GetObject("FormLogo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap passwordlogo {
             get {
                 object obj = ResourceManager.GetObject("passwordlogo", resourceCulture);
