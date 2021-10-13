9856fb218c053ba340a8a6a08ca9a9d895ad120e
Miguel Correia <correiapora@gmail.com>
2021-10-13 16:05:10 +0200

    Creating text files

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,9 +15,26 @@ namespace PRG282_Milestone_2
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LogInForm());
+
+            // To create a text file for the logIn and the details.
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new LogInForm());
+
+            //string path1 = @"C:\Users\corre\Desktop\Study\Second year subjects\PRG 282\Project\Milestone_2\PRG282_Project_Milestone2\PRG282_Milestone_2\bin\Debug\LoginDetails.txt";
+            //string path2 = @"C:\Users\corre\Desktop\Study\Second year subjects\PRG 282\Project\Milestone_2\PRG282_Project_Milestone2\PRG282_Milestone_2\bin\Debug\LoginTimes.txt";
+
+            //string loginDetail = "Miguel;Miguel123";
+            //string loginTime = "Miguel;16:00 pm";
+
+            //File.WriteAllText(path1, loginDetail);
+            //File.WriteAllText(path2, loginTime);
+
+            //Console.ReadLine();
+
+
+
         }
     }
 }
